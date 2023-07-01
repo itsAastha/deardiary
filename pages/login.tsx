18028dea11f340a05ac7c296c9f317dcb79ea321
Lava Kumar Gowda M A <91609093+nameisluv@users.noreply.github.com>
2023-07-02 01:02:42 +0530

    routing, auth and session management done

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../public/assets/img/loggedin.svg';
 import img2 from '../public/assets/img/loginpage.svg';
+import { useRouter } from 'next/router';
 
 import Image, { StaticImageData } from "next/image";
 
@@ -8,40 +9,41 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Login = (): JSX.Element => {
   const { data:session } = useSession();
-
+  const router = useRouter();
   if (session) {
-    return (
-      <div className="bg-white">
-        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="py-8 px-4 sm:rounded-lg sm:px-10 flex items-center flex-col">
-              <div className="mb-3">
-              <Image
-                src={img1}
-                alt={""}
-                className="w-100 rounded-lg transition"
-              />
-              </div>
-              <div>
-                <h1 className="text-black">Email: {session.user.email}</h1>
-              </div>
-              <div>
-                <h1 className="text-black">Name: {session.user.name}</h1>
-              </div>
-              <div className="mt-4">
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  onClick={() => signOut()}
-                >
-                  Sign Out
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    router.push('/dashboard');
+    // return (
+    //   <div className="bg-white">
+    //     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+    //       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    //         <div className="py-8 px-4 sm:rounded-lg sm:px-10 flex items-center flex-col">
+    //           <div className="mb-3">
+    //           <Image
+    //             src={img1}
+    //             alt={""}
+    //             className="w-100 rounded-lg transition"
+    //           />
+    //           </div>
+    //           <div>
+    //             <h1 className="text-black">Email: {session.user.email}</h1>
+    //           </div>
+    //           <div>
+    //             <h1 className="text-black">Name: {session.user.name}</h1>
+    //           </div>
+    //           <div className="mt-4">
+    //             <button
+    //               type="submit"
+    //               className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    //               onClick={() => signOut()}
+    //             >
+    //               Sign Out
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
   } else {
     return (
       <div>
