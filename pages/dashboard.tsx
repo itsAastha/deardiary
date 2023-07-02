07dc8f332fa023ed8e2701513939ad82b801689f
Aastha <aastha2333@gmail.com>
2023-07-02 19:12:39 +0530

    dashboard and diary added

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,29 +1,119 @@
-import { useSession } from 'next-auth/react';
-import Layout from '@/components/layout';
-import Sidebar from '@/components/sidebar'
-import { useRouter } from 'next/router';
+import { useSession } from "next-auth/react";
+import Layout from "@/components/layout";
+import Sidebar from "@/components/sidebar";
+import { useRouter } from "next/router";
+
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const features = [
+  { name: "Origin", description: "Designed by Good Goods, Inc." },
+  {
+    name: "Material",
+    description:
+      "Solid walnut base with rare earth magnets and powder coated steel card cover",
+  },
+  { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
+  { name: "Finish", description: "Hand sanded and finished with natural oil" },
+  { name: "Includes", description: "Wood card tray and 3 refill packs" },
+  {
+    name: "Considerations",
+    description:
+      "Made from natural materials. Grain and color vary with each item.",
+  },
+];
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
   const router = useRouter();
   {
-    if (status === 'loading') {
+    if (status === "loading") {
       return <div>Loading...</div>;
     }
-    
-    if (session){
+
+    if (session) {
       return (
         <>
-        <Layout>
-            <div className='lg:ml-64'>
-                Holadddddddddddddddddddddddddddddddddddddd
+          <Layout>
+            <div className="lg:ml-64">
+              <div className="mx-10">
+                <div className="">
+                  <div className="mx-10">
+                    <div className="bg-white">
+                      <div className="mx-auto grid  grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-10  lg:grid-cols-2 lg:px-4">
+                        <div>
+                          <h2 className="text-2xl font-bold tracking-tight text-lime-500 ">
+                            These are the most common emotions you've experienced lately.
+                          </h2>
+                          <p className="mt-4 text-gray-500">
+                           From the diary entries you've made in the past few days' we've compiled the emotions that you've experienced and have prepared and analysis for you.
+                          </p>
+
+                          <dl className="mt-5 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+                            {features.map((feature) => (
+                              <div
+                                key={feature.name}
+                                className="border-t border-gray-200 pt-4"
+                              >
+                                <dt className="font-medium text-gray-900">
+                                  {feature.name}
+                                </dt>
+                                <dd className="mt-2 text-sm text-gray-500">
+                                  {feature.description}
+                                </dd>
+                              </div>
+                            ))}
+                          </dl>
+                        </div>
+                        <div className="grid grid-cols-2  grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+                          <div className="w-64 h-64 items-center flex justify-center   bg-pink-100">
+                            g1
+                          </div>
+                          <div className="w-64 h-64 items-center flex justify-center bg-pink-100">
+                            g1
+                          </div>
+                          <div className="w-64 h-64 items-center flex justify-center bg-pink-100">
+                            g1
+                          </div>
+                          <div className="w-64 h-64 items-center flex justify-center bg-pink-100">
+                            g1
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </Layout>
+          </Layout>
         </>
-      )
-    };
-    router.push('/login');
-  return null;
+      );
+    }
+    router.push("/login");
+    return null;
   }
-  
 }
--- pages/diary.tsx
+++ pages/diary.tsx
@@ -2,7 +2,63 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import Layout from '@/components/layout';
 import Sidebar from '@/components/sidebar'
 import { useRouter } from 'next/router';
+import { Fragment } from 'react'
+import {
+  CalendarIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  EllipsisHorizontalIcon,
+  MapPinIcon,
+} from '@heroicons/react/20/solid'
+import { Menu, Transition } from '@headlessui/react'
 
+const days = [
+    { date: '2021-12-27' },
+    { date: '2021-12-28' },
+    { date: '2021-12-29' },
+    { date: '2021-12-30' },
+    { date: '2021-12-31' },
+    { date: '2022-01-01', isCurrentMonth: true },
+    { date: '2022-01-02', isCurrentMonth: true },
+    { date: '2022-01-03', isCurrentMonth: true },
+    { date: '2022-01-04', isCurrentMonth: true },
+    { date: '2022-01-05', isCurrentMonth: true },
+    { date: '2022-01-06', isCurrentMonth: true },
+    { date: '2022-01-07', isCurrentMonth: true },
+    { date: '2022-01-08', isCurrentMonth: true },
+    { date: '2022-01-09', isCurrentMonth: true },
+    { date: '2022-01-10', isCurrentMonth: true },
+    { date: '2022-01-11', isCurrentMonth: true },
+    { date: '2022-01-12', isCurrentMonth: true, isToday: true },
+    { date: '2022-01-13', isCurrentMonth: true },
+    { date: '2022-01-14', isCurrentMonth: true },
+    { date: '2022-01-15', isCurrentMonth: true },
+    { date: '2022-01-16', isCurrentMonth: true },
+    { date: '2022-01-17', isCurrentMonth: true },
+    { date: '2022-01-18', isCurrentMonth: true },
+    { date: '2022-01-19', isCurrentMonth: true },
+    { date: '2022-01-20', isCurrentMonth: true },
+    { date: '2022-01-21', isCurrentMonth: true },
+    { date: '2022-01-22', isCurrentMonth: true, isSelected: true },
+    { date: '2022-01-23', isCurrentMonth: true },
+    { date: '2022-01-24', isCurrentMonth: true },
+    { date: '2022-01-25', isCurrentMonth: true },
+    { date: '2022-01-26', isCurrentMonth: true },
+    { date: '2022-01-27', isCurrentMonth: true },
+    { date: '2022-01-28', isCurrentMonth: true },
+    { date: '2022-01-29', isCurrentMonth: true },
+    { date: '2022-01-30', isCurrentMonth: true },
+    { date: '2022-01-31', isCurrentMonth: true },
+    { date: '2022-02-01' },
+    { date: '2022-02-02' },
+    { date: '2022-02-03' },
+    { date: '2022-02-04' },
+    { date: '2022-02-05' },
+    { date: '2022-02-06' },
+  ]
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
 export default function Diary() {
   const { data: session, status } = useSession();
   const router = useRouter();
@@ -15,8 +71,78 @@ export default function Diary() {
       return (
         <>
         <Layout>
-            <div className='lg:ml-64'>
-                Holadddddddddddddddddddddddddddddddddddddd
+            <div className='lg:ml-64 mr-10'>
+                
+            <h2 className="text-lg font-semibold text-gray-900"></h2>
+      <div className="lg:grid lg:grid-cols-12 lg:gap-x-16">
+        <div className="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-9">
+          <div className="flex items-center text-gray-900">
+            <button
+              type="button"
+              className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">Previous month</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            <div className="flex-auto font-semibold">January</div>
+            <button
+              type="button"
+              className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">Next month</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+            <div>M</div>
+            <div>T</div>
+            <div>W</div>
+            <div>T</div>
+            <div>F</div>
+            <div>S</div>
+            <div>S</div>
+          </div>
+          <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
+            {days.map((day, dayIdx) => (
+              <button
+                key={day.date}
+                type="button"
+                className={classNames(
+                  'py-1.5 hover:bg-gray-100 focus:z-10',
+                  day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
+                  (day.isSelected || day.isToday) && 'font-semibold',
+                  day.isSelected && 'text-white',
+                  !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900',
+                  !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400',
+                  day.isToday && !day.isSelected && 'text-indigo-600',
+                  dayIdx === 0 && 'rounded-tl-lg',
+                  dayIdx === 6 && 'rounded-tr-lg',
+                  dayIdx === days.length - 7 && 'rounded-bl-lg',
+                  dayIdx === days.length - 1 && 'rounded-br-lg'
+                )}
+              >
+                <time
+                  dateTime={day.date}
+                  className={classNames(
+                    'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
+                    day.isSelected && day.isToday && 'bg-indigo-600',
+                    day.isSelected && !day.isToday && 'bg-gray-900'
+                  )}
+                >
+                  {day.date.split('-').pop().replace(/^0/, '')}
+                </time>
+              </button>
+            ))}
+          </div>
+          <button
+            type="button"
+            className="mt-8 w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Add event
+          </button>
+        </div>
+        
+      </div>
             </div>
         </Layout>
         </>
